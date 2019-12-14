5d67142c8ad215de7f1da4d9a04192c0466b101d
Josh Shor <jpshor@gmail.com>
2019-12-14 11:50:50 -0300

    feat(windows): adds Node 11+ support

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -1,4 +1,4 @@
-if ($env:APPVEYOR_REPO_BRANCH -eq 'master') {
+if ($env:APPVEYOR_REPO_BRANCH -eq 'master' -and $env:APPVEYOR_PULL_REQUEST_NUMBER -eq '') {
     try {
         yarn publish:binary
     } catch { "OK" }
