ee628c4c3fa9700d662ad5ef18a57b2092e470de
Josh Shor <jpshor@gmail.com>
2019-12-14 19:25:13 -0300

    fix(appveyor): update script to include debug msg and wrap conditionals

--- scripts/appveyor.ps1
+++ scripts/appveyor.ps1
@@ -1,5 +1,3 @@
-if ($env:APPVEYOR_REPO_BRANCH -eq 'master' -and $env:APPVEYOR_PULL_REQUEST_NUMBER -eq '') {
-    try {
-        yarn publish:binary
-    } catch { "OK" }
-}
+try {
+    yarn publish:binary
+} catch { "OK" }
