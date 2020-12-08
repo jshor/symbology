7be6b2eb6d965d70379c146528ce1b3503afa5da
Josh Shor <jpshor@gmail.com>
2020-12-08 15:37:17 -0500

    chore: updates zint to 2.9.1

--- scripts/install.js
+++ scripts/install.js
@@ -2,6 +2,7 @@ const clone = require('git-clone')
 const replace = require('replace-in-file')
 const rimraf = require('rimraf')
 const path = require('path')
+const fs = require('fs')
 const patches = require('./patches')
 const { zintVersion } = require('../package.json')
 
@@ -19,6 +20,17 @@ rimraf(path.join(__dirname, '../.zint'), () => {
       console.log('Successfully cloned. Applying code patches...')
 
       patches.forEach(patch => replace.sync(patch))
+
+      console.log('Creating zint config header file...')
+
+      const configData = `
+#define ZINT_VERSION_MAJOR 2
+#define ZINT_VERSION_MINOR 9
+#define ZINT_VERSION_RELEASE 1
+#define ZINT_VERSION_BUILD 9
+`
+
+      fs.writeFileSync('./.zint/backend/zintconfig.h', configData)
 
       console.log('Done.')
     }
