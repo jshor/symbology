96e433c47f66779060f05e1e348a61fdc6cdb5e4
Tim Haak <tim@haak.co>
2023-05-25 20:36:07 +0200

    Fix test

--- src/lib/__tests__/binary.test.ts
+++ src/lib/__tests__/binary.test.ts
@@ -1,36 +1,32 @@
-import EncodingMode from '../../types/enums/EncodingMode'
-import OutputType from '../../types/enums/OutputType'
-import OutputOption from '../../types/enums/OutputOption'
-import binding from '../../binding'
-import binary from '../binary'
+import EncodingMode from "../../types/enums/EncodingMode"
+import OutputType from "../../types/enums/OutputType"
+import OutputOption from "../../types/enums/OutputOption"
+import binding from "../../binding"
+import binary from "../binary"
+import {SymbologyType} from "../../index"
 
-describe('Binary methods', () => {
+describe("Binary methods", () => {
   afterEach(() => jest.resetAllMocks())
 
-  describe('createBuffer()', () => {
+  describe("createBuffer()", () => {
     beforeEach(() => {
       jest
-        .spyOn(binding, 'createStream')
+        .spyOn(binding, "createStream")
         .mockReturnValue({
-          message: 'Barcode created',
-          code: 2,
-          width: 32,
-          height: 32,
-          bitmap: [],
-          encodedData: ''
+          message: "Barcode created", code: 2, width: 32, height: 32, bitmap: [], encodedData: ""
         })
     })
 
-    it('should call createStream() with the arguments provided by the config', () => {
+    it("should call createStream() with the arguments provided by the config", () => {
       const symbol = {
         symbology: 10,
         height: 30,
         whitespaceWidth: 3,
         borderWidth: 5,
         outputOptions: OutputOption.SMALL_TEXT,
-        foregroundColor: '00ff00ff',
-        backgroundColor: 'ff0000ff',
-        fileName: 'myfile.svg',
+        foregroundColor: "00ff00ff",
+        backgroundColor: "ff0000ff",
+        fileName: "myfile.svg",
         scale: 1.3,
         option1: 1,
         option2: 2,
@@ -38,49 +34,28 @@ describe('Binary methods', () => {
         showHumanReadableText: false,
         encoding: EncodingMode.DATA_MODE,
         eci: 0,
-        primary: '',
-        text: 'example text',
+        primary: "",
+        text: "example text",
         rotation: 0,
         dotSize: 0.8
       }
-      const barcodeData = 'primary data'
+      const barcodeData = "primary data"
 
       binary.createBuffer(symbol, barcodeData)
 
-      expect(binding.createStream).toHaveBeenCalledWith(
-        barcodeData,
-        symbol.symbology,
-        symbol.height,
-        symbol.whitespaceWidth,
-        symbol.borderWidth,
-        symbol.outputOptions,
-        symbol.backgroundColor,
-        symbol.foregroundColor,
-        symbol.fileName,
-        symbol.scale,
-        symbol.option1,
-        symbol.option2,
-        symbol.option3,
-        0,
-        'example text',
-        symbol.encoding,
-        symbol.eci,
-        symbol.primary,
-        symbol.rotation,
-        symbol.dotSize
-      )
+      expect(binding.createStream).toHaveBeenCalledWith(barcodeData, symbol.symbology, symbol.height, symbol.whitespaceWidth, symbol.borderWidth, symbol.outputOptions, symbol.backgroundColor, symbol.foregroundColor, symbol.fileName, symbol.scale, symbol.option1, symbol.option2, symbol.option3, 0, "example text", symbol.encoding, symbol.eci, symbol.primary, symbol.rotation, symbol.dotSize)
     })
 
-    it('should fallback to default values for values missing in config', () => {
+    it("should fallback to default values for values missing in config", () => {
       const symbol = {
         symbology: 10,
         height: 30,
         whitespaceWidth: 3,
         borderWidth: 5,
         outputOptions: 2,
-        foregroundColor: '00ff00ff',
-        backgroundColor: 'ff0000ff',
-        fileName: 'myfile.svg',
+        foregroundColor: "00ff00ff",
+        backgroundColor: "ff0000ff",
+        fileName: "myfile.svg",
         scale: 1.3,
         option1: 1,
         option2: 2,
@@ -88,108 +63,74 @@ describe('Binary methods', () => {
         showHumanReadableText: true,
         encoding: EncodingMode.DATA_MODE,
         eci: 0,
-        primary: '',
-        text: '',
+        primary: "",
+        text: "",
         rotation: 0,
         dotSize: 0.8
       }
-      const barcodeData = 'primary data'
+      const barcodeData = "primary data"
 
       binary.createBuffer(symbol, barcodeData)
 
-      expect(binding.createStream).toHaveBeenCalledWith(
-        barcodeData,
-        symbol.symbology,
-        symbol.height,
-        symbol.whitespaceWidth,
-        symbol.borderWidth,
-        symbol.outputOptions,
-        symbol.backgroundColor,
-        symbol.foregroundColor,
-        symbol.fileName,
-        symbol.scale,
-        symbol.option1,
-        symbol.option2,
-        symbol.option3,
-        1,
-        barcodeData,
-        symbol.encoding,
-        symbol.eci,
-        symbol.primary,
-        symbol.rotation,
-        symbol.dotSize
-      )
+      expect(binding.createStream).toHaveBeenCalledWith(barcodeData, symbol.symbology, symbol.height, symbol.whitespaceWidth, symbol.borderWidth, symbol.outputOptions, symbol.backgroundColor, symbol.foregroundColor, symbol.fileName, symbol.scale, symbol.option1, symbol.option2, symbol.option3, 1, barcodeData, symbol.encoding, symbol.eci, symbol.primary, symbol.rotation, symbol.dotSize)
     })
   })
 
-  describe('invoke()', () => {
-    describe('when symbol creation succeeds without warnings', () => {
+  describe("invoke()", () => {
+    describe("when symbol creation succeeds without warnings", () => {
       const result = {
-        code: 0,
-        message: 'Success',
-        bitmap: [],
-        encodedData: '<encoded data>',
-        width: 10,
-        height: 15
+        code: 0, message: "Success", bitmap: [], encodedData: "<encoded data>", width: 10, height: 15
       }
 
       beforeEach(() => {
         jest
-          .spyOn(binary, 'createBuffer')
+          .spyOn(binary, "createBuffer")
           .mockReturnValue(result)
       })
 
-      it('should resolve with the resulting binary data when symbology is successfully rendered', async () => {
+      it("should resolve with the resulting binary data when symbology is successfully rendered", async () => {
         expect.assertions(1)
 
         await expect(binary.invoke({
-          symbology: 10
-        }, '12345', OutputType.SVG)).resolves.toEqual({
-          ...result,
-          message: 'Symbology successfully created.'
+          symbology: SymbologyType.CODE128
+        }, "12345", OutputType.SVG)).resolves.toEqual({
+          ...result, message: "Symbology successfully created."
         })
       })
 
-      it('should apply bitmap output option (8) to existing output options for non-PNG images', async () => {
+      it("should apply bitmap output option (8) to existing output options for non-PNG images", async () => {
         const config = {
-          symbology: 10,
-          outputOptions: OutputOption.BARCODE_BIND
+          symbology: SymbologyType.CODE128, outputOptions: OutputOption.BARCODE_BIND
         }
-        const barcodeData = '12345'
+        const barcodeData = "12345"
 
         await binary.invoke(config, barcodeData, OutputType.SVG)
 
         expect(binary.createBuffer).toHaveBeenCalledTimes(1)
         expect(binary.createBuffer).toHaveBeenCalledWith({
-          ...config,
-          fileName: 'out.svg',
-          outputOptions: config.outputOptions + 8
+          ...config, fileName: "out.svg", outputOptions: config.outputOptions + 8
         }, barcodeData)
       })
 
-      it('should set the output options to bitmap output (8) to non-PNG images wth no previous output specified', async () => {
+      it("should set the output options to bitmap output (8) to non-PNG images wth no previous output specified", async () => {
         const config = {
-          symbology: 10,
-          outputOptions: 0
+          symbology: 10, outputOptions: 0
         }
-        const barcodeData = '12345'
+        const barcodeData = "12345"
 
         await binary.invoke(config, barcodeData, OutputType.SVG)
 
         expect(binary.createBuffer).toHaveBeenCalledTimes(1)
         expect(binary.createBuffer).toHaveBeenCalledWith({
-          ...config,
-          fileName: 'out.svg',
-          outputOptions: 8
+          ...config, fileName: "out.svg", outputOptions: 8
         }, barcodeData)
       })
 
-      it('should not mutate the output options for PNG images', async () => {
+      it("should not mutate the output options for PNG images", async () => {
         const config = {
-          symbology: 10,
-          outputOptions: OutputOption.BARCODE_BIND
+          symbology: 10, outputOptions: OutputOption.BARCODE_BIND
         }
-        const barcodeData = '12345'
+        const barcodeData = "12345"
 
         await binary.invoke(config, barcodeData, OutputType.PNG)
 
@@ -200,68 +141,58 @@ describe('Binary methods', () => {
       })
     })
 
-    describe('when symbol creation succeeds with a warning', () => {
-      it('should resolve if the status code is greater than 2', async () => {
+    describe("when symbol creation succeeds with a warning", () => {
+      it("should resolve if the status code is greater than 2", async () => {
         const result = {
-          code: 1,
-          message: 'Created with warnings',
-          bitmap: [],
-          encodedData: '<encoded data>',
-          width: 10,
-          height: 15
+          code: 1, message: "Created with warnings", bitmap: [], encodedData: "<encoded data>", width: 10, height: 15
         }
 
         jest
-          .spyOn(binary, 'createBuffer')
+          .spyOn(binary, "createBuffer")
           .mockReturnValue(result)
 
         expect.assertions(1)
 
         await expect(binary.invoke({
-          symbology: 10
-        }, '12345', OutputType.SVG)).resolves.toEqual(result)
+          symbology: SymbologyType.CODE128
+        }, "12345", OutputType.SVG)).resolves.toEqual(result)
       })
     })
 
-    describe('when the symbol creation fails', () => {
-      it('should reject if the status code is greater than 2', async () => {
+    describe("when the symbol creation fails", () => {
+      it("should reject if the status code is greater than 2", async () => {
         const result = {
-          code: 3,
-          message: 'Failure',
-          bitmap: [],
-          encodedData: '<encoded data>',
-          width: 10,
-          height: 15
+          code: 3, message: "Failure", bitmap: [], encodedData: "<encoded data>", width: 10, height: 15
         }
 
         jest
-          .spyOn(binary, 'createBuffer')
+          .spyOn(binary, "createBuffer")
           .mockReturnValue(result)
 
         expect.assertions(1)
 
         await expect(binary.invoke({
-          symbology: 10
-        }, '12345', OutputType.SVG)).rejects.toEqual(result.message)
+          symbology: SymbologyType.CODE128
+        }, "12345", OutputType.SVG)).rejects.toEqual(result.message)
       })
     })
   })
 
-  describe('getOutputType()', () => {
-    it('should return PNG for a file with a .png extension', () => {
-      expect(binary.getOutputType('out.PNG')).toEqual(OutputType.PNG)
+  describe("getOutputType()", () => {
+    it("should return PNG for a file with a .png extension", () => {
+      expect(binary.getOutputType("out.PNG")).toEqual(OutputType.PNG)
     })
 
-    it('should return SVG for a file with a .svg extension', () => {
-      expect(binary.getOutputType('out.SVG')).toEqual(OutputType.SVG)
+    it("should return SVG for a file with a .svg extension", () => {
+      expect(binary.getOutputType("out.SVG")).toEqual(OutputType.SVG)
     })
 
-    it('should return EPS for a file with a .eps extension', () => {
-      expect(binary.getOutputType('out.EPS')).toEqual(OutputType.EPS)
+    it("should return EPS for a file with a .eps extension", () => {
+      expect(binary.getOutputType("out.EPS")).toEqual(OutputType.EPS)
     })
 
-    it('should fall back to PNG for an unrecognized file extension', () => {
-      expect(binary.getOutputType('out.tiff')).toEqual(OutputType.PNG)
+    it("should fall back to PNG for an unrecognized file extension", () => {
+      expect(binary.getOutputType("out.tiff")).toEqual(OutputType.PNG)
     })
   })
 })
