3b7904dddf8f1e2abc50b8910423d8fba4770e7e
Josh Shor <jpshor@gmail.com>
2023-10-15 10:12:45 -0400

    feat(node): upgrades to support Node 18, 20 #120
    BREAKING CHANGE: This drops support for Node.js v14.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import SymbologyType from './types/enums/SymbologyType'
-import DataMatrix from './types/enums/DataMatrix'
-import EncodingMode from './types/enums/EncodingMode'
-import OutputOption from './types/enums/OutputOption'
-import OutputType from './types/enums/OutputType'
-import { createStream, createFile } from './main'
-
 export { default as SymbologyType } from './types/enums/SymbologyType'
 export { default as DataMatrix } from './types/enums/DataMatrix'
 export { default as EncodingMode } from './types/enums/EncodingMode'
@@ -13,17 +6,3 @@ export { default as OutputType } from './types/enums/OutputType'
 export { default as SymbologyConfig } from './types/SymbologyConfig'
 export { default as SymbologyResult } from './types/SymbologyResult'
 export { createStream, createFile } from './main'
-
-export default {
-  DataMatrix,
-  EncodingMode,
-  OutputOption,
-  OutputType,
-  SymbologyType,
-  createStream,
-  createFile,
-  /** Legacy support - remove in next major release */
-  Encoding: EncodingMode,
-  Output: OutputOption,
-  Barcode: SymbologyType
-}
